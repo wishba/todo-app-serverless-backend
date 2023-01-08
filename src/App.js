a2d97e6b36586af350009856cd0ef3848547e224
wishba <wisnubayu34@gmail.com>
2023-01-08 20:32:34 +0700

    gidu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEffect } from 'react';
-import netlifyIdentity from 'netlify-identity-widget'
 
 function App() {
-  useEffect(() => {
-    netlifyIdentity.init()
-  }, [])
-
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <button onClick={() => {
-            netlifyIdentity.open()
-            netlifyIdentity.on('login', (user) => {
-              console.log(user.id)
-            })
-          }}>Login</button>
-          <button onClick={() => netlifyIdentity.logout()}>Logout</button>
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
