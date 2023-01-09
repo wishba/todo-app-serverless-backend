dbf374d55ec2041cfd35d75bc04f3c2326e83479
wishba <wisnubayu34@gmail.com>
2023-01-09 12:59:23 +0700

    id send via url

--- netlify/functions/allTodoById.js
+++ netlify/functions/allTodoById.js
@@ -1,21 +1,12 @@
 const sendQuery = require("./utils/sendQuery")
 
-const GET_TODOS = `
-  query {
-    allTodoById(netlify_id: "1") {
-      data {
-        _id
-        netlify_id
-        activity
-        completed
-      }
-    }
-  } 
-`
+const GET_TODOS = `query($netlify_id: String!) { allTodoById(netlify_id: $netlify_id) { data { _id netlify_id activity completed } } }`
 
-exports.handler = async () => {
+exports.handler = async (event) => {
   try {
-    const response = await sendQuery(GET_TODOS)
+    const netlify_id = event.queryStringParameters.netlify_id
+    const variables = { netlify_id }
+    const response = await sendQuery(GET_TODOS, variables)
     const data = response.allTodoById.data
     return {
       statusCode: 200,
