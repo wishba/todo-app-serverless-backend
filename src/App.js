89193d855dac2757e81c05c28e6b8bedc668fb06
wishba <wisnubayu34@gmail.com>
2023-01-10 12:45:20 +0700

    render when login only

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import netlifyIdentity from 'netlify-identity-widget'
 
 function App() {
+  const [thereIsUser, setThereIsUser] = useState()
+
   useEffect(() => {
     netlifyIdentity.init()
+
+    if (netlifyIdentity.currentUser() !== null) {
+      console.log(netlifyIdentity.currentUser());
+      setThereIsUser(true)
+    }
+
+    netlifyIdentity.on('login', () => {
+      netlifyIdentity.close()
+      setThereIsUser(true)
+    })
+
+    netlifyIdentity.on('logout', () => {
+      setThereIsUser(false)
+    })
   }, [])
 
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={() => netlifyIdentity.open()}>Login - Register</button>
-        <button onClick={() => netlifyIdentity.logout()}>Logout</button>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Login to see your todo</p>
+        {
+          thereIsUser ? (
+            <button onClick={() => netlifyIdentity.logout()}>Logout</button>
+          ) : (
+            <div>
+              <div>
+                <button onClick={() => netlifyIdentity.open()}>Login - Register</button>
+              </div>
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>Login to see your todo</p>
+            </div>
+          )
+        }
       </header>
     </div>
   );
