abd031823df516d651c3a25cbc58f59d947185fa
wishba <wisnubayu34@gmail.com>
2023-01-11 12:19:41 +0700

    create todo manual

--- src/components/FormCreateTodo.js
+++ src/components/FormCreateTodo.js
@@ -1,7 +1,21 @@
 import React from 'react'
 
 function FormCreateTodo() {
-  const handleCreateTodo = async event => event.preventDefault()
+  const handleCreateTodo = async event => {
+    event.preventDefault()
+
+    try {
+      await fetch('/.netlify/functions/createTodo', {
+        method: 'POST',
+        body: JSON.stringify({
+          netlify_id: "14a3b21b-1df9-4070-b35c-a03dfa183458",
+          activity: "test 12:17"
+        })
+      })
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   return (
     <form onSubmit={handleCreateTodo}>
