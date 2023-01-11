a3cc2ae7eb041d0e1199aad5810c670592613979
wishba <wisnubayu34@gmail.com>
2023-01-11 17:59:05 +0700

    run allTodo if id !null

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import netlifyIdentity from 'netlify-identity-widget'
-import FormCreateTodo from './components/FormCreateTodo'
 
 function App() {
-  const [netlifyId, setNetlifyId] = useState('')
-  const [allTodo, setAllTodo] = useState([])
+  netlifyIdentity.init()
 
   const allTodoById = async () => {
     try {
       const response = await fetch('/.netlify/functions/allTodoById', {
         method: 'PUT',
         body: JSON.stringify({
-          netlify_id: netlifyId
+          netlify_id: netlifyIdentity.currentUser().id
         })
       })
       const data = await response.json()
-      setAllTodo(data)
 
       console.log(data)
 
@@ -24,41 +21,140 @@ function App() {
     }
   }
 
-  netlifyIdentity.on('login', user => {
-    setNetlifyId(user.id)
-  })
-
-  netlifyIdentity.on('logout', () => {
-    setNetlifyId('')
-  })
-
   useEffect(() => {
     if (netlifyIdentity.currentUser() !== null) {
-      setNetlifyId(netlifyIdentity.currentUser().id)
+      allTodoById()
     }
-
-    allTodoById()
-  }, [netlifyId])
+  }, [])
 
   return (
     <div>
-      <button onClick={() => netlifyIdentity.open()}>Login / Logout</button>
+      <button onClick={() => netlifyIdentity.open()}>Login / Register</button>
       <button onClick={() => netlifyIdentity.logout()}>Logout</button>
-
-      <FormCreateTodo />
-
-      <ul>
-        {allTodo.map((todo) => (
-          <li key={todo._id}>
-            <p>{todo.completed ? ('Completed') : ('Uncompleted')} - {todo.activity}</p>
-          </li>
-        ))}
-      </ul>
     </div>
   )
 }
 
-export default App;
+export default App
+
+
+
+// import React, { useEffect, useState } from 'react'
+// import netlifyIdentity from 'netlify-identity-widget'
+
+// function App() {
+  // const [netlifyId, setNetlifyId] = useState('')
+  // const [allTodo, setAllTodo] = useState([])
+
+  // netlifyIdentity.on('login', user => {
+    // setNetlifyId(user.id)
+  // })
+
+  // netlifyIdentity.on('logout', () => {
+    // setNetlifyId('')
+  // })
+
+  // const allTodoById = async () => {
+  //   try {
+  //     const response = await fetch('/.netlify/functions/allTodoById', {
+  //       method: 'PUT',
+  //       body: JSON.stringify({
+  //         netlify_id: netlifyId
+  //       })
+  //     })
+  //     const data = await response.json()
+  //     setAllTodo(data)
+
+  //     console.log(data)
+
+  //   } catch (error) {
+  //     console.error(error)
+  //   }
+  // }
+
+  // const allTodoById = () => {
+    // fetch('/.netlify/functions/allTodoById', {
+      // method: 'PUT',
+      // body: JSON.stringify({
+        // netlify_id: netlifyId
+      // })
+    // })
+      // .then(response => response.json())
+      // .then(data => {
+        // setAllTodo(data)
+        // console.log(data)
+      // })
+      // .catch(error => console.error(error));
+  // }
+  // allTodoById()
+
+  // const [activity, setActivity] = useState('')
+  // const [newTodo, setNewTodo]=useState()
+  // const handleCreateTodo = async event => {
+    // event.preventDefault()
+    // try {
+      // await fetch('/.netlify/functions/createTodo', {
+        // const response = await fetch('/.netlify/functions/createTodo', {
+        // method: 'POST',
+        // body: JSON.stringify({
+          // netlify_id: netlifyIdentity.currentUser().id,
+          // activity: activity
+        // })
+      // })
+      // const data = await response.json()
+
+      // console.log(data);
+      // setNewTodo({
+      //   _id: '',
+      //   netlify_id: netlifyIdentity.currentUser().id,
+      //   activity: activity,
+      //   completed:false
+      // })
+
+      // allTodoById()
+
+    // } catch (error) {
+      // console.error(error);
+    // }
+  // }
+
+  // useEffect(() => {
+    // if (netlifyIdentity.currentUser() !== null) {
+      // setNetlifyId(netlifyIdentity.currentUser().id)
+    // }
+
+    // allTodoById()
+
+//   }, [netlifyId])
+
+//   return (
+//     <div>
+//       <button onClick={() => netlifyIdentity.open()}>Login / Logout</button>
+//       <button onClick={() => netlifyIdentity.logout()}>Logout</button>
+
+//       <form onSubmit={handleCreateTodo}>
+//         <label htmlFor="">Activity:</label>
+//         <input
+//           type="text"
+//           name=""
+//           id=""
+//           onChange={event => setActivity(event.target.value)}
+//         />
+//         <input type="submit" value="Create Todo" />
+//       </form>
+
+//       <ul>
+//         {allTodo.map((todo) => (
+//           <li key={todo._id}>
+//             <p>{todo.completed ? ('Completed') : ('Uncompleted')} - {todo.activity}</p>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   )
+// }
+
+// export default App;
 
 // import logo from './logo.svg';
 // import './App.css';
--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import netlifyIdentity from 'netlify-identity-widget'
 
-netlifyIdentity.init()
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
