047df291e33fba12c063c61c2f4d65cc2238dde8
wishba <wisnubayu34@gmail.com>
2024-02-20 16:37:32 +0700

    fetch todo with userId

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,37 @@ import netlifyIdentity from 'netlify-identity-widget';
 import { useEffect, useState } from 'react';
 
 function App() {
-  const [userName, setUserName] = useState('')
+  const [userName, setUserName] = useState()
+
+  const fetchTodo = async (userId) => {
+    try {
+      const response = await fetch('/.netlify/functions/todosRead', {
+        method: 'POST',
+        body: JSON.stringify({
+          userId
+        })
+      })
+
+      const data = await response.json()
+      console.log(data);
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   useEffect(() => {
     if (netlifyIdentity.currentUser() != null) {
       setUserName(netlifyIdentity.currentUser().user_metadata.full_name)
       console.log(netlifyIdentity.currentUser().id);
+      fetchTodo(netlifyIdentity.currentUser().id)
     }
 
     netlifyIdentity.on('login', user => {
       setUserName(user.user_metadata.full_name)
       netlifyIdentity.close()
+      console.log(netlifyIdentity.currentUser().id);
+      fetchTodo(netlifyIdentity.currentUser().id)
     })
 
     netlifyIdentity.on('logout', () => {
@@ -29,8 +49,6 @@ function App() {
       </button>
 
       <h1>Hello{userName ? ` ${userName}!` : '!'}</h1>
-
-      <a href={`/.netlify/functions/hello?name=${userName}`} target="_blank" rel="noopener noreferrer">test netlify function</a>
     </div>
   )
 }
