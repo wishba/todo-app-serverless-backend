09b737c4cdd656f775e2d5306439a8956dbcb3ff
wishba <wisnubayu34@gmail.com>
2024-02-21 16:59:51 +0700

    handleUpdate

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ function App() {
   const [allTodo, setAllTodo] = useState()
   const [isLoadingTodo, setIsLoadingTodo] = useState(false)
   const [todoField, setTodoField] = useState()
+  const [updateId, setUpdateId] = useState()
+  const [updateTodoField, setUpdateTodoField] = useState()
+  const [updateFinishedField, setUpdateFinishedField] = useState(false)
 
   const fetchTodo = async () => {
     setIsLoadingTodo(true)
@@ -72,9 +75,30 @@ function App() {
     }
   }
 
-  const handleUpdate = todo => {
-    console.log('update id: ' + todo.ref['@ref'].id);
-    console.log(todo);
+  const handleUpdate = async e => {
+    e.preventDefault()
+
+    try {
+      await fetch('.netlify/functions/todosUpdate', {
+        method: 'PUT',
+        body: JSON.stringify({
+          todoId: updateId,
+          todo: updateTodoField,
+          finished: updateFinishedField
+        })
+      })
+
+      fetchTodo()
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const handleUpdateInfo = todo => {
+    setUpdateId(todo.ref['@ref'].id)
+    setUpdateTodoField(todo.data.todo)
+    setUpdateFinishedField(todo.data.finished)
   }
 
   const handleDelete = async todo => {
@@ -108,25 +132,36 @@ function App() {
         <input type="submit" value="create" />
       </form>
 
-      {
-        isLoadingTodo ?
-          (
-            <p>Loading...</p>
-          ) :
-          (
-            <ul>
-              {
-                allTodo?.map(todo => (
-                  <li key={todo.ref['@ref'].id}>
-                    <p>Todo: {todo.data.todo}</p>
-                    <p>Finished: {JSON.stringify(todo.data.finished)}</p>
-                    <button onClick={() => handleUpdate(todo)}>Update</button>
-                    <button onClick={() => handleDelete(todo)}>Delete</button>
-                  </li>
-                )).slice().reverse()
-              }
-            </ul>
-          )
+      <form onSubmit={e => handleUpdate(e)}>
+        <input type="text"
+          value={updateTodoField}
+          onChange={e => setUpdateTodoField(e.target.value)}
+        />
+        <input type="checkbox"
+          checked={updateFinishedField}
+          onChange={() => setUpdateFinishedField(!updateFinishedField)}
+        />
+        <input type="submit" value="update" />
+      </form>
+
+      {isLoadingTodo ?
+        (
+          <p>Loading...</p>
+        ) :
+        (
+          <ul>
+            {
+              allTodo?.map(todo => (
+                <li key={todo.ref['@ref'].id}>
+                  <p>Todo: {todo.data.todo}</p>
+                  <p>Finished: {JSON.stringify(todo.data.finished)}</p>
+                  <button onClick={() => handleUpdateInfo(todo)}>Update</button>
+                  <button onClick={() => handleDelete(todo)}>Delete</button>
+                </li>
+              )).slice().reverse()
+            }
+          </ul>
+        )
       }
     </div>
   )
